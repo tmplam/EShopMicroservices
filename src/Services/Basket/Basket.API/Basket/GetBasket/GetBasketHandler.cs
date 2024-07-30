241189ad8bc1a92b465c9395389bd7d038f27612
tmplam <tranmyphulam123@gmail.com>
2024-07-30 16:02:26 +0700

    implement base of basket service

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        return await Task.FromResult(new GetBasketResult(new ShoppingCart("tmplam")));
+    }
+}
---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,7 @@
+﻿global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.CQRS;
+global using Carter;
+global using FluentValidation;
+global using Mapster;
+global using MediatR;
