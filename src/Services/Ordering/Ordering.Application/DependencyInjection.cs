2226cfc77de2a03c9b4094d5c10a843cc1c4dd4a
tmplam <tranmyphulam123@gmail.com>
2024-08-05 21:36:31 +0700

    complete ordering application layer

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
 
@@ -11,6 +12,8 @@ public static class DependencyInjection
         services.AddMediatR(conf =>
         {
             conf.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            conf.AddOpenBehavior(typeof(ValidationBehavior<,>));
+            conf.AddOpenBehavior(typeof(LoggingBehavior<,>));
         });
 
         return services;
